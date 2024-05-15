60ddcbf0f1176e82a1d5412d0c014b284e65b95b
marshallharris <marshall.harris3@gmail.com>
2024-05-14 20:52:32 -0600

    add area as a parameter of Target
    also add helpers to output a string and the percentage of the image area

--- Target.py
+++ Target.py
@@ -4,8 +4,16 @@ from dataclasses import dataclass
 class Target:
     x_center: float
     y_center: float
+    area: float
     width: float
     height: float
     rho: float
     phi: float
     timestamp: float
+
+    def stringForImage(self):
+        return f"x: {self.x_center}, y: {self.y_center}, w: {self.width}, h: {self.height}, rho:{self.rho}"
+
+    def percentOfImage(self):
+        imageArea = 640 * 480
+        return self.area * 100 / imageArea
